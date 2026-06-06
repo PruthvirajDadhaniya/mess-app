3e743a82521ac5ebf742a8a3d5b60e4f8fcf3a3a
Sanket <sanketkhot7774@gmail.com>
2026-06-06 14:02:48 +0530

    mess-app create price and time page

--- src/app/main/messowner/mess-owner-dashboard/dashboard/price/price.ts
+++ src/app/main/messowner/mess-owner-dashboard/dashboard/price/price.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-price',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './price.html',
-  styleUrl: './price.css',
+  styleUrls: ['./price.css']
 })
 export class Price {
+  priceData = {
+    monthlyMembership: '3500 Rs',
+    singleMeal: '100 Rs',
+    specialVeg: '150 Rs',
+    specialNonVeg: '200 Rs'
+  };
 
+  onEdit() {
+    console.log('Edit Price clicked');
+  }
+
+  onSave() {
+    console.log('Save Price clicked:', this.priceData);
+  }
 }
