a55659d7443d3c18cb0a07ff294e63f51a797052
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2026-06-10 21:59:27 +0530

    mess-app create a menu side drawer and styling

--- src/app/main/customer/customer-home/content/content.ts
+++ src/app/main/customer/customer-home/content/content.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MenuDrawer } from "./menu-drawer/menu-drawer";
 
 @Component({
   selector: 'app-content',
@@ -10,17 +11,21 @@ import { MatIconModule } from '@angular/material/icon';
     CommonModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
-  ],
+    MatIconModule,
+    MenuDrawer
+],
   templateUrl: './content.html',
   styleUrl: './content.css',
 })
 export class Content {
+   @ViewChild('menuDrawer') menuDrawer!: MenuDrawer;
+
   visibleCount = 6;
 
   chefs = Array.from({ length: 30 }, (_, i) => ({
     id: i + 1,
     name: 'Kolhapur Khanaval',
+    subtitle: 'Authentic Punjabi Cuisine',
     image: i % 3 === 0
       ? './assets/images/food1.jpg'
       : i % 3 === 1
@@ -28,11 +33,16 @@ export class Content {
         : './assets/images/food3.jpg',
     rating: 4.1,
     reviews: 397,
-    address: '925, Main road, Bazar lane,\n Hadapsar, Pune',
-    distance: '0.8 km',
+    address: '925, Main road, Bazar lane, Hadapsar, Pune',
+    distance: '0.8 km away',
     timing: '11:00 AM to 2:00 PM',
     evening: '7:15 PM to 9:30 PM',
-    phone: '+91 987456123'
+    phone: '+91 987456123',
+    liked: false,
+    heroImg: './assets/images/food1.jpg',
+    menuImg: './assets/images/food2.jpg',
+    foodType: '🌿 Pure Veg',
+    contact: '+91 987456123',
   }));
 
   get displayedChefs() {
@@ -45,6 +55,12 @@ export class Content {
 
   onImageError(event: Event) {
     const img = event.target as HTMLImageElement;
-    img.src = './assets/images/thali-1.jpg'; // Fallback image if original fails to load
+    img.src = './assets/images/thali-1.jpg';
+  }
+
+  openDrawer(chef: any): void {
+     console.log('Menu Clicked', chef);
+    this.menuDrawer.loadChef(chef);
+    this.menuDrawer.open();
   }
 }
